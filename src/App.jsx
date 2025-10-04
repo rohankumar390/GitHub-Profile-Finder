8eb77ffbf15f0172f50409d8cae9814d61dd495d
rohankumar390 <rohanyt74@gmail.com>
2025-10-04 17:24:58 +0530

    removing auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import { useState, useEffect } from "react";
-import GitHubProfiles from "./GitHubProfiles"; // Import the GitHubProfiles component
-import Login from "./login";
+import GitHubProfiles from "./GitHubProfiles";
+// import Login from "./login";
 import {
   HashRouter as Router,
   Route,
-  Routes,
-  Navigate,
+  Routes
+  // Navigate,
 } from "react-router-dom";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false); // Manage authentication state
+  // const [isAuthenticated, setIsAuthenticated] = useState(false); // Manage authentication state
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [searchInput, setSearchInput] = useState(""); // Keep the input state here
   const [searchType, setSearchType] = useState("location"); // Set 'location' as default search type
 
-  useEffect(() => {
-    const token = localStorage.getItem("accessToken");
-    if (token) {
-      setIsAuthenticated(true); // Auto-authenticate if the token is found
-    }
-  }, []);
+  // useEffect(() => {
+  //   const token = localStorage.getItem("accessToken");
+  //   if (token) {
+  //     setIsAuthenticated(true); // Auto-authenticate if the token is found
+  //   }
+  // }, []);
 
   // Toggle dark mode class on the body element
   useEffect(() => {
@@ -30,73 +30,56 @@ function App() {
     }
   }, [isDarkMode]);
 
-  const handleLogin = (token) => {
-    localStorage.setItem("accessToken", token);
-    setIsAuthenticated(true);
-  };
+  // const handleLogin = (token) => {
+  //   localStorage.setItem("accessToken", token);
+  //   setIsAuthenticated(true);
+  // };
 
-  const handleLogout = () => {
-    localStorage.removeItem("accessToken");
-    setIsAuthenticated(false);
-  };
+  // const handleLogout = () => {
+  //   localStorage.removeItem("accessToken");
+  //   setIsAuthenticated(false);
+  // };
 
   return (
     <Router>
       <div className="container">
         <Routes>
           <Route
-            path="/login"
-            element={
-              isAuthenticated ? (
-                <Navigate to="/" />
-              ) : (
-                <Login onLogin={handleLogin} />
-              )
-            }
-          />
-
-          <Route
             path="/"
             element={
-              isAuthenticated ? (
-                <>
-                  <div>
-                    <button onClick={handleLogout} className="logout-button">
-                      Logout
-                    </button>
-                    <h1
-                      className="title dark-mode-toggle"
-                      onClick={() => setIsDarkMode(!isDarkMode)}
-                    >
-                      GitHub Finder
-                    </h1>
-                  </div>
-                  <div className="sticky-container">
-                    {/* Search input field */}
-                    <input
-                      type="text"
-                      value={searchInput}
-                      onChange={(e) => setSearchInput(e.target.value)} // Manage the search input
-                      placeholder={`Search GitHub profiles by ${searchType}`} // Dynamically update placeholder
-                    />
-
-                    <select
-                      value={searchType}
-                      onChange={(e) => setSearchType(e.target.value)}
-                    >
-                      <option value="location">Location</option>
-                      <option value="username">Username</option>
-                      <option value="fullname">Name</option>
-                    </select>
-                  </div>
-                  <GitHubProfiles
-                    searchInput={searchInput}
-                    searchType={searchType}
+              <>
+                <div>
+                  <h1
+                    className="title dark-mode-toggle"
+                    onClick={() => setIsDarkMode(!isDarkMode)}
+                  >
+                    GitHub Finder
+                  </h1>
+                </div>
+                <div className="sticky-container">
+                  {/* Search input field */}
+                  <input
+                    type="text"
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)}
+                    placeholder={`Search GitHub profiles by ${searchType}`}
                   />
-                </>
-              ) : (
-                <Navigate to="/login" />
-              )
+
+                  <select
+                    value={searchType}
+                    onChange={(e) => setSearchType(e.target.value)}
+                  >
+                    <option value="location">Location</option>
+                    <option value="username">Username</option>
+                    <option value="fullname">Name</option>
+                  </select>
+                </div>
+
+                <GitHubProfiles
+                  searchInput={searchInput}
+                  searchType={searchType}
+                />
+              </>
             }
           />
         </Routes>
